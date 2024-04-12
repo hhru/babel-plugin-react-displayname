1abcaf7af56ad80b8f9066484efd51e6829b93c6
Nikita Bezdolniy <gooverdian@gmail.com>
2024-04-12 03:13:44 +0300

    HH-212842-testing

--- test/fixtures/exportDefaultCallExpression/expected.js
+++
@@ -1,5 +0,0 @@
-import { connect } from 'react-redux';
-import { jsx as _jsx } from "react/jsx-runtime";
-const _generatedConstant = () => /*#__PURE__*/_jsx("div", {});
-_generatedConstant.displayName = "fixtures/exportDefaultCallExpression/input/NoName";
-export default connect(null, {})(_generatedConstant);
