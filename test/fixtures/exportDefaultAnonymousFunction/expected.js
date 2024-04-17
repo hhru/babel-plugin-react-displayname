c32caa9d49c81d2796637ce57bfcc0358ab96ef6
Nikita Bezdolniy <gooverdian@gmail.com>
2024-04-17 12:13:06 +0300

    one-more-thing-testing

--- test/fixtures/exportDefaultAnonymousFunction/expected.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function _generatedConstant({
-  value
-}) {
-  return /*#__PURE__*/_jsx("p", {
-    children: value
-  });
-}
-_generatedConstant.displayName = "fixtures/exportDefaultAnonymousFunction/input/NoName";
--- test/fixtures/hocFunctionComponent/input.js
+++
@@ -1,7 +0,0 @@
-export const HocFunctionFragmentComponent = translation(({ trls }) => (
-    <>{trls}</>
-));
-
-const HocFunctionElementComponent = translation(({ trls }) => (
-    <div>{trls}</div>
-));
