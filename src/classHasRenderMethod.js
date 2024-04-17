dede83f8464dd71150f938df6ebfd0ebf4b9163a
Nikita Bezdolniy <gooverdian@gmail.com>
2024-04-17 13:02:48 +0300

    HH-213926-testing

--- src/classHasRenderMethod.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(path) {
-    if (!path.node.body) {
-        return false;
-    }
-
-    return path.node.body.body.some(({ type, key }) => type == 'ClassMethod' && key.name == 'render');
-};
--- test/fixtures/exportDefaultAnonymousArrowFunction/input.js
+++
@@ -1,3 +0,0 @@
-export default ({ value }) => {
-  return <p>{value}</p>
-}
